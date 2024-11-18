81110a081d181442cdebed16721e207e6c7ffa00
helenafig <60613757+helenafig@users.noreply.github.com>
2024-11-18 15:37:53 -0300

    Add files via upload

--- waffle_chart_helena.R
+++ waffle_chart_helena.R
@@ -53,94 +53,13 @@ data <- aggregate(quantity ~ waste_type, data, sum)
 data <- mutate(data , quantity_MM = round(quantity / 1000000 , digits = 2))
 data <- mutate(data , waste_type = fct_reorder(waste_type, quantity_MM))
 data <- mutate(data , perc = paste0(sprintf("%4.1f", quantity_MM / sum(quantity_MM) * 100), "%"))
-
-graph <- ggplot(data, aes(quantity_MM, waste_type))
-
-graph <- graph + 
-  geom_col(aes(quantity_MM, waste_type), fill = "#076fa2", width = 0.6) +
-  scale_x_continuous(
-    limits = c(0, 75.5),
-    breaks = seq(0, 80, by = 10), 
-    expand = c(0, 0), # The horizontal axis does not extend to either side
-    position = "top"  # Labels are located on the top
-  ) +
-  scale_y_discrete(expand = expansion(add = c(0, 0.5))) +
-  theme(
-    panel.background = element_rect(fill = "white"),
-    panel.grid.major.x = element_line(color = "#A8BAC4", size = 0.3),
-    axis.ticks.length = unit(0, "mm"),
-    #axis.title = element_blank(),
-    axis.line.y.left = element_line(color = "black"),
-    axis.text.y = element_blank(),
-    axis.text.x = element_text(family = "Econ Sans Cnd", size = 16),
-    plot.title = element_text(
-      family = "Econ Sans Cnd", 
-      face = "bold",
-      size = 22
-    ),
-    plot.subtitle = element_text(
-      family = "Econ Sans Cnd",
-      size = 20
-    ),
-    axis.title.x = element_text(size = 15, face = "bold"),
-    axis.title.y = element_text(size = 15, face = "italic"),
-    plot.caption =  element_text(
-      family = "Econ Sans Cnd",
-      size = 16
-    )
-  ) +
-  geom_text(aes(label = perc), hjust = 0, size = 7 , colour = "#076fa2" , family = "Econ Sans Cnd") +
-  geom_text(
-    aes(0, y = waste_type, label = waste_type),
-    hjust = 0,
-    nudge_x = 0.3,
-    colour = "white",
-    family = "Econ Sans Cnd",
-    size = 5
-  ) +
-  labs(
-    title = "Source of Food Waste in EU",
-    subtitle = "Total in Tonnes of Waste Food (MM), 2022",
-    x = "Quantity of Food Waste (MM of Tonnes)",
-    y = "Source of Food Waste",
-    caption = "Source: Eurostat"
-  )
-
-graph
-
-install.packages("echarts4r")
-library(echarts4r)
-remotes::install_github("JohnCoene/echarts4r.assets")
-library(echarts4r.assets) #remotes::install_github("JohnCoene/echarts4r.assets")
-
-style <- list(
-  normal = list(opacity = 1),
-  # normal
-  emphasis = list(opacity = 1) # on hover
-)
-
-#E_Charts
-
-data %>% 
-  e_charts(waste_type) %>% 
-  e_pictorial(quantity_MM, symbol = ea_icons("trash"), 
-              symbolRepeat = TRUE, z = 0,
-              itemStyle = style,
-              symbolSize = c(20, 30)) %>% 
-  e_theme("westeros") %>%
-  e_title("Source of Food Waste in EU - Total in Tonnes of Waste Food (MM), 2022") %>% 
-  e_flip_coords() %>%
-  # Hide Legend
-  e_legend(show = FALSE) %>%
-  # Remove Gridlines
-  e_x_axis(splitLine=list(show = FALSE)) %>%
-  e_y_axis(splitLine=list(show = FALSE)) %>%
-  # Format Label
-  e_labels(fontSize = 26, fontWeight ='bold', position = "right", offset=c(10, 0))
+data <- mutate(data , percentage = quantity_MM / sum(quantity_MM))
 
 
 # Waffle plot: 
-my_font <- "Roboto Condensed"
+my_font <- "Roboto"
+
+
 
 
 # Define a shared color palette for the categories
@@ -157,7 +76,7 @@ data <- data %>%
   mutate(label_x = c(4, 9, 11, 12.5, 14),  # X positions for the labels
          label_y = rep(8, n()),      # Place all labels in the 8th row (above the graph)
          label_color = category_colors[waste_type],
-         label_text = paste0(round(quantity_MM, 1), "MM\n(", round(percentage, 0), "%)"))  # Assign colors from the palette
+         label_text = paste(round(quantity_MM, 1), "MM")) # Assign colors from the palette
 
 data
 color_palette
@@ -171,14 +90,14 @@ wa_plot <-
   scale_x_continuous(breaks = NULL) +  # Remove gridlines
   labs(title = "**54% of food waste in EU comes from Households**", 
        subtitle = "Total in Tonnes of Waste Food (MM), 2022:<br>
-                  <span style='color:#CC8E51FF;'>**Households**</span>,
-                  <span style='color:#51A3CCFF;'>**Industries**</span>,
                   <span style='color:#A3CC51FF;'>**Production (agriculture)**</span>,
-                  <span style='color:#6551CCFF;'>**Restaurants**</span> and
-                  <span style='color:#CC5151FF;'>**Retail and commerce**</span>.") +
+                  <span style='color:#CC5151FF;'>**Retail and commerce**</span>,
+                  <span style='color:#6551CCFF;'>**Restaurants**</span>,
+                  <span style='color:#51A3CCFF;'>**Industries**</span> and 
+                  <span style='color:#CC8E51FF;'>**Households**</span>.") +
   theme(text = element_text(family = my_font), 
         plot.title = element_markdown(color = "#292929"), 
-        plot.subtitle = element_markdown(color = "#292929", margin = margin(b = 20)),
+        plot.subtitle = element_markdown(color = "#292929", margin = margin(b = 5)),
         axis.text = element_blank(),  # Remove axis labels
         axis.ticks = element_blank(),  # Remove axis ticks
         plot.background = element_rect("#EBEBEB"),
@@ -186,22 +105,36 @@ wa_plot <-
         axis.title = element_blank(),
         legend.position = "none") +  # Remove axis titles
   geom_text(data = data, 
-            aes(x = 4, y = 7.8, label = label_text[1], colour = waste_type[1]),  # Use positions and category colors
-            size = 3.5, inherit.aes = FALSE) +
+            aes(x = 1, y = 0.15, label = label_text[1], colour = waste_type[1]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
   geom_text(data = data, 
-            aes(x = 9, y = 7.8, label = label_text[2], colour = waste_type[2]),  # Use positions and category colors
-            size = 3.5, inherit.aes = FALSE) +
+            aes(x = 1, y = -0.2, label = perc[1], colour = waste_type[1]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
   geom_text(data = data, 
-            aes(x = 11, y = 7.8, label = label_text[3], colour = waste_type[3]),  # Use positions and category colors
-            size = 3.5, inherit.aes = FALSE) +
+            aes(x = 3, y = 0.15, label = label_text[2], colour = waste_type[2]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
   geom_text(data = data, 
-            aes(x = 12.5, y = 7.8, label = label_text[4], colour = waste_type[4]),  # Use positions and category colors
-            size = 3.5, inherit.aes = FALSE) +
+            aes(x = 3, y = -0.2, label = perc[2], colour = waste_type[2]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
   geom_text(data = data, 
-            aes(x = 14, y = 7.8, label = label_text[5], colour = waste_type[5]),  # Use positions and category colors
-            size = 3.5, inherit.aes = FALSE)  +
+            aes(x = 4, y = 0.15, label = label_text[3], colour = waste_type[3]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
+  geom_text(data = data, 
+            aes(x = 4, y = -0.2, label = perc[3], colour = waste_type[3]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
+  geom_text(data = data, 
+            aes(x = 6, y = 0.15, label = label_text[4], colour = waste_type[4]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
+  geom_text(data = data, 
+            aes(x = 6, y = -0.2, label = perc[4], colour = waste_type[4]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE) +
+  geom_text(data = data, 
+            aes(x = 12, y = 0.15, label = label_text[5], colour = waste_type[5]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE)  +
+  geom_text(data = data, 
+            aes(x = 12, y = -0.2, label = perc[5], colour = waste_type[5]),  # Use positions and category colors
+            size = 3.5, inherit.aes = TRUE)  +
   scale_colour_manual(values = category_colors) +  # Use shared color palette for text
   coord_fixed(ratio = 1)  # Ensure squares instead of rectangles
 
 wa_plot
-warnings()
