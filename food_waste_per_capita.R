664aa702831980f0e8ed724b595db870613e5b4d
Dani Gil M <36381277+dansrvx@users.noreply.github.com>
2024-11-18 20:28:47 +0000

    Dani

---
+++ food_waste_per_capita.R
@@ -0,0 +1,101 @@
+food_waste_per_capita <- function(data, custom_theme) {
+  library(ggplot2)
+  library(sf)
+  library(dplyr)
+  library(readxl)
+  library(tidyverse)
+  library(rnaturalearth)
+  library(rnaturalearthdata)
+  library(data.table)
+  library(plotly)
+  library(lubridate)
+  library(dplyr)
+  library(grid)
+  library(shadowtext)
+  library(gghighlight)
+  library(ggflags)
+  library(countrycode)
+  #remotes::install_github('rensa/ggflags')
+  
+  #### Select Data Set
+  data <- filter(data, metric == "kg_per_capita" & year == "2022")
+  data <- data[rowSums(is.na(data)) < 2 , ]
+  
+  #### Second PLOT Ranking of Food Waste Per Capita
+  # Select Data
+  data <- data %>%
+    dplyr::select(country , households) %>% arrange(desc(households))
+  
+  # Calculate total waste
+  data <- mutate(data, country = fct_reorder(country, households))
+  
+  ranking <- filter(data,
+                    row_number() <= 5 |
+                      row_number() >= n() - 5 | country == "European Union")
+  ranking <- mutate(ranking, waste_ranking = as.factor(ifelse(
+    row_number() <= 5,
+    "top_5",
+    ifelse(
+      country == "European Union",
+      "European_Union",
+      ifelse(row_number() >= n() - 5, "last_5", "")
+    )
+  )))
+  ranking <- mutate(ranking, countrycode = tolower(ifelse(
+    country == "European Union",
+    "EU",
+    countrycode(country, origin = 'country.name', destination = 'iso2c')
+  )))
+  
+  graph <- ggplot(ranking, aes(households, country))
+  
+  graph <- graph +
+    geom_col(aes(x = households, y = country, fill = waste_ranking), width = 0.6) +
+    geom_flag(x = 18, aes(country = countrycode), size = 10) +
+    scale_fill_manual(
+      name = "Ranking of Food Waste per Country",
+      labels = c("Top 5 Countries", "European Union", "Last 5 Counties"),
+      values = c(
+        "top_5" = "#B54057",
+        "European_Union" = "gray",
+        "last_5" = "#048A81"
+      )
+    ) +
+    scale_x_continuous(
+      limits = c(0, 130),
+      breaks = seq(0, 140, by = 10),
+      expand = c(0, 0),
+      # The horizontal axis does not extend to either side
+      position = "top"  # Labels are located on the top
+    ) +
+    scale_y_discrete(expand = expansion(add = c(0, 0.5))) +
+    custom_theme +
+    geom_text(
+      aes(label = households),
+      hjust = 0,
+      size = 7  ,
+      family = "Econ Sans Cnd"
+    ) +
+    geom_text(
+      aes(0, y = country, label = country),
+      hjust = 0,
+      nudge_x = 0.3,
+      colour = "white",
+      family = "Econ Sans Cnd",
+      face = "bold",
+      size = 5
+    ) +
+    labs(
+      title = "Food Waste Per Capita in EU",
+      subtitle = "Total in KG of Waste Food per Capita, 2022",
+      x = "Quantity of Food Waste per Capita (KG)",
+      y = "Country",
+      caption = "Source: Eurostat"
+    )
+  
+  return(graph)
+}
+
+
+
+
---
+++ main.R
@@ -0,0 +1,144 @@
+  library(readxl)
+  library(shiny)
+  source("food_waste_per_capita.R")
+  source("biomass_waste.R")
+  source("source_food_waste.R")
+  source("waffle_food_waste.R")
+  
+  
+  custom_theme <- theme(
+    panel.background = element_rect(fill = "white"),
+    panel.grid.major.x = element_line(color = "#A8BAC4", size = 0.3),
+    axis.ticks.length = unit(0, "mm"),
+    axis.line.y.left = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    # Titles
+    plot.title = element_text(
+      family = "Nunito",
+      size = 35,
+      face = "bold",
+      hjust = 0.5
+    ),
+    # Subtitles
+    plot.subtitle = element_text(
+      family = "Nunito",
+      size = 25,
+      hjust = 0.5
+    ),
+    # Legend title
+    legend.title = element_text(family = "Nunito", size = 20),
+    # Legend text
+    legend.text = element_text(family = "Nunito", size = 18),
+    # Axis text
+    axis.text = element_text(family = "Nunito", size = 15),
+    # Axis title
+    axis.title = element_text(family = "Nunito", size = 15),
+    legend.key.size = unit(1, "cm"),  # Agrandar los cuadros de la leyenda
+    axis.text.x = element_text(size = 14, family = "Nunito", color = "black"),  # Configurar texto del eje X
+    # axis.text.y = element_text(size = 14, family = "Nunito", color = "black")   # Configurar texto del eje Y
+  )
+  
+  
+  # Draw plots
+  waste_data <- read_excel("data/df_foodwaste.xlsx")
+  disposal_data <- read_excel("data/df_waste_destination.xlsx")
+  plot1 <- food_waste_per_capita(waste_data, custom_theme)
+  plot1
+  plot3 <- treemap_by_country(disposal_data, custom_theme)
+  plot3
+  plot2 <- total_food_waste(waste_data, custom_theme)
+  plot2
+  plot4 <- treemap_by_type(disposal_data, custom_theme)
+  plot4
+  plot5 <- waffle_food_waste(waste_data, custom_theme)
+  plot5
+  
+  # Interfaz de usuario
+library(shiny)
+
+# Define la interfaz de usuario
+ui <- navbarPage(
+  title = "Menu",
+  # Agrega la fuente Nunito desde Google Fonts
+  header = tags$head(
+    tags$h1("Food waste and allocation analysis - European Union - 2022", align = "center"),
+    tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap"),
+    tags$style(HTML("
+      body {
+        font-family: 'Nunito', sans-serif;
+      }
+      h1, h2, h3, h4, h5, h6 {
+        font-weight: 700;
+      }
+      .navbar-brand {
+        font-size: 24px;
+        font-weight: 700;
+      }
+    "))
+  ),
+  # Página 1
+  tabPanel("Waste Generation", 
+           fluidPage(
+             h4("General rankings on food waste generation"),
+             fluidRow(
+               column(6, plotOutput("plot1a", height = "800px", width = "1000px")),  # Primer gráfico
+               column(6, plotOutput("plot1b", height = "800px", width = "1000px"))   # Segundo gráfico
+             )
+           )),
+  
+  # Página 2
+  tabPanel("Biomass Allocation", 
+           fluidPage(
+             h4("Biomass Allocation in the UE"),
+             fluidRow(
+               column(6, plotOutput("plot2a", height = "800px", width = "1000px")),  # Primer gráfico
+               column(6, plotOutput("plot2b", height = "800px", width = "1000px"))   # Segundo gráfico
+             )
+           )),
+  
+  # Página 3
+  tabPanel("Waste Disposal", 
+           fluidPage(
+             h4("Biomass Disposal in the UE"),
+             fluidRow(
+               column(6, plotOutput("plot3a", height = "800px", width = "1000px")),  # Primer gráfico
+               column(6, plotOutput("plot3b", height = "800px", width = "1000px"))   # Segundo gráfico
+             )
+           ))
+  
+)
+
+# Define el servidor
+server <- function(input, output) {
+  # Gráficos de la Página 1
+  output$plot1a <- renderPlot({
+    plot1  # Primer gráfico
+  })
+  
+  output$plot1b <- renderPlot({
+    plot2  # Segundo gráfico
+  })
+  
+  # Gráficos de la Página 2
+  output$plot2a <- renderPlot({
+    plot3  # Primer gráfico
+  })
+  
+  output$plot2b <- renderPlot({
+    plot4  # Segundo gráfico
+  })
+  
+  # Gráficos de la Página 3
+  output$plot3a <- renderPlot({
+    plot5  # Primer gráfico
+  })
+
+}
+
+
+# Ejecuta la aplicación
+shinyApp(ui = ui, server = server)
+  
+  
+
+
